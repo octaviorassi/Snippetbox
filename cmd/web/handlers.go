1fa089f4198cda0199c4ba54ab7cdd981b4a2aa6
Octavio <octaviorassi@gmail.com>
2024-12-15 19:17:32 -0300

    general progress

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+
+	// Create a slice with the paths to the html files
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	// Using files... rather than files makes the contents of the slice be passed as variadic arguments (separated)
+	// ParseFiles takes an arbitrary amount of file paths and returns a *Template object, which we store in ts.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// ExecuteTemplate is a method for the Template object which executes the template and writes it into the
+	// io.Writer object passed as its first argument. In this case, it is executing the template (replacing the
+	// placeholders with the actual contents) and then writing it as a response to the http request through w.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 
 }
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
+
+
+	// given an http.Handler, StripPrefix generates another one that serves the same purpose but removes the 
+	// given prefix from the request's path.
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
