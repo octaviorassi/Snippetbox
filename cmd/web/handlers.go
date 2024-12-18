52be0ace12893d289c2b48198951583e33c3015f
Octavio <octaviorassi@gmail.com>
2024-12-18 02:09:04 -0300

    updated snippetCreatePost to actually read the form input and correctly store the snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"snippetbox.octaviorassi.net/internal/models"
 )
@@ -28,7 +30,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-		// Extract the id from the path's wildcard value
+	// Extract the id from the path's wildcard value
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		app.clientError(w, http.StatusNotFound)
@@ -55,14 +57,55 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+
+	data := app.newTemplateData(r)
+
+	app.render(w, r, http.StatusOK, "create.tmpl.html", data)
+	
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Example with dummy variables which we will later replace
-	title 	:= "O snail"
-	content := "O snail\nClimb Mount FUji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	expires := 7
+	
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title 	:= r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Initializes a map for error storage
+	fieldErrors := make(map[string]string)
+
+	// title must be non-empty and 100 characters at most
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	// content cannot be blank
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	// expires must match one of the permitted values (1, 7, 365)
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must be equal to 1, 7, or 365"
+	}
+
+	// Check if any errors were registered
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
 
 	// We now call the snippet.Insert from the app.DB 
 	id, err := app.snippets.Insert(title, content, expires)
